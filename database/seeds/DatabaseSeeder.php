b49ae083f52e6f1bfde50c9c9c1d542572fce6c4
Flávio Martins <fhcmartins@gmail.com>
2019-03-27 22:03:17 -0300

    Aula 10  - 27/03/2019

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(ProvidersTableSeeder::class);
+        $this->call(ClassificationsTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
     }
 }
--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,14 +11,28 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create();
+        
+        $classificationsIDs = DB::table('classifications')
+            ->select('id')
+            ->get();
+        
+        $providersIDs = DB::table('providers')
+            ->select('id')
+            ->get();
+
+
         for ($i=1; $i <= 20 ; $i++) {
-            $faker = Faker\Factory::create();
 
             DB::table('products')->insert([
                     'descricao' => $faker->text(100),
                     'qtd' => $faker->randomNumber(),
                     'prc_venda' => $faker->randomFloat(2, 100, 200),
                     'prc_compra' => $faker->randomFloat(2, 100, 200),
+
+                    'providers_id' => $faker->randomElement($providersIDs)->id,
+                    'classifications_id' => $faker->randomElement($classificationsIDs)->id,
+
                     'created_at' => \Carbon\Carbon::now()
             ]);
         }
